dcd1d05e7fbe042cfe26545bec196c4adb16d208
qwartz <qwartzemail@gmail.com>
2017-09-02 13:25:02 -0500

    Complete katoolin update

--- katoolin.py
+++ katoolin.py
@@ -1,7 +1,25 @@
 #!/usr/bin/env	python
+# -*- coding: utf-8 -*-
 
 import sys,os
-from core import katoolin
+from core import gear
 
-if __name__ == "__main__":
-	katoolin.user_put()
+def user_put():
+	os.system('clear')
+	gear.banner()
+	action = False
+	while action == False:
+		try:
+			user_input = raw_input(": katoolin > ")
+		except KeyboardInterrupt:
+			print "\nClosing, bye! - katoolin"
+			sys.exit()
+		if user_input in gear.menu_list:
+			gear.load(gear.menu_list[user_input])
+		elif "=" in user_input:
+			user_input = user_input.split('=')
+			if "load" == user_input[0]:
+				gear.load_category(user_input[1])
+
+if __name__ == '__main__':
+	user_put()
