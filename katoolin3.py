bcfc4f1bfa3c8e9a818a2ed83437f550a32cc529
s-h-3-l-l <s-h-3-l-l@git>
2019-11-12 20:03:46 +0100

    Added LICENSE

--- katoolin3.py
+++ katoolin3.py
@@ -1,5 +1,22 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+#
+# katoolin3 - Install Kali packages on your Debian/Ubuntu machine
+# Copyright (C) 2019 s-h-3-l-l
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 """
 katoolin3 is a port of katoolin for python3.
