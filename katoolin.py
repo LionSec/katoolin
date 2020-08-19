d5a580c4000ee9f873b92aa692e0f2675ea07716
Ian Galope <galopeian@gmail.com>
2020-08-18 20:08:56 -0400

    Update katoolin.py
    fixed typos

--- katoolin.py
+++ katoolin.py
@@ -5,7 +5,7 @@ import sys, traceback
 
 
 if os.getuid() != 0:
-	print "Sorry. This script requires sudo privledges"
+	print "Sorry. This script requires sudo privleges"
 	sys.exit()
 def main():
 	try:
